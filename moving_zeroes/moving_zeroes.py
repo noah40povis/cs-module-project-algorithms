d63fc17d8f03d89ed448658fab66213fffa07136
Noah Christian <noahchristian@Noahs-MacBook-Pro.local>
2020-09-16 21:11:56 -0400

    finished moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,12 +3,16 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    lst = []
+    for i in arr: 
+        if i != 0:
+            lst.insert(0, i)
+        else:
+            lst.insert(len(arr), i)
+    return lst 
 
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == '__main__': 
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
